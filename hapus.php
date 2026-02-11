acf8dfdf32acd9f488a15a243527fa849fe7cacd
cinta2009 <21cintatata@gmail.com>
2026-02-11 07:51:24 +0700

    update hapus

--- hapus.php
+++ hapus.php
@@ -1,8 +1,11 @@
 <?php
 include 'config.php';
 
-$id = $_GET['id'];
-mysqli_query($koneksi, "DELETE FROM tbsiswa WHERE id='$id'");
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    // Menghapus data berdasarkan ID yang dipilih
+    mysqli_query($koneksi, "DELETE FROM tb_izin WHERE id='$id'");
+}
 
 header("Location: view-data.php");
 exit;
