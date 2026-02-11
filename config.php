0e153246f6f48e70e59c8c4a11a2f5b957af83c3
cinta2009 <21cintatata@gmail.com>
2026-02-11 07:49:11 +0700

    update

--- config.php
+++ config.php
@@ -2,6 +2,9 @@
 // Host, User, Password, Nama Database
 $koneksi = mysqli_connect("localhost", "root", "root", "dbsekolah");
 
+// TAMBAHKAN BARIS INI:
+date_default_timezone_set('Asia/Jakarta'); 
+
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
